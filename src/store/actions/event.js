e3ac8cfddf0d1d6196f7385c525ca8db9aa8b3a3
dziq45 <piotr.papriko@gmail.com>
2020-04-28 15:29:22 +0200

    Little store changes

--- src/store/actions/event.js
+++ src/store/actions/event.js
@@ -1,15 +1,12 @@
 import axios from 'axios'
 import * as actionTypes from './actionTypes'
 
-export const setid = (eventId)=>{
-    return {
-        type:actionTypes.SET_ACTIVE_EVENT,
-        activeEventId : eventId
-    }
-}
 export const setActiveEventId = (eventId)=>{
     console.log('siema')
     return dispatch=>{
-        dispatch(setid(eventId))
+        dispatch({
+            type:actionTypes.SET_ACTIVE_EVENT,
+            activeEventId : eventId 
+        })
     }
 }
