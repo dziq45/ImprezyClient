23ed8bdf9a63d7b3c356fab9fa7a9b00ce8d71ed
dziq45 <piotr.papriko@gmail.com>
2020-04-29 13:53:08 +0200

    EventBoard logic

--- src/components/Event/EventBoard.js
+++ src/components/Event/EventBoard.js
@@ -3,6 +3,11 @@ import axios from 'axios'
 import {connect} from 'react-redux'
 import './eventCss/Event.css'
 import Colaboration from './Colaboration'
+import Schedule from './Schedule'
+import CostOrganizer from './CostOrganizer'
+import Planning from './Planning'
+import Publication from './Publication'
+import EventForm from './EventForm'
 class EventBoard extends Component{
     state={
         activeItem : 1
@@ -10,25 +15,41 @@ class EventBoard extends Component{
     componentDidMount(){
         console.log(this.props.eventId)
     }
+    renderProperItem(){
+        switch(this.state.activeItem){
+            case 0:
+                //return(<EventForm eventId={this.props.eventId}></EventForm>)
+            case 1:
+                return(<Schedule eventId={this.props.eventId}></Schedule>)
+            case 2:
+                return(<Planning eventId={this.props.eventId}></Planning>)
+            case 3:
+                return(<Colaboration eventId={this.props.eventId}></Colaboration>)
+            case 4:
+                return(<CostOrganizer eventId={this.props.eventId}></CostOrganizer>)
+            case 5:
+                return(<Publication eventId={this.props.eventId}></Publication>)
+        }
+    }
     render(){
         return(
             <div className="flex  mx-6">
                 <div className="flex-2 border mx-2 bg-gray-200">
                     <ul>
                         <li>
-                            <div className="boardOptionItem">
+                            <div className="boardOptionItem" onClick={()=>{this.setState({activeItem : 0})}}>
                                 Ogólne
                             </div>
                         </li>
-                        <li><div className="boardOptionItem">Harmonogram</div></li>
-                        <li><div className="boardOptionItem">Planowanie</div></li>
-                        <li><div className="boardOptionItem">Kolaboranci</div></li>
-                        <li><div className="boardOptionItem">Kosztorys</div></li>
-                        <li><div className="boardOptionItem">Publikacja</div></li>
+                        <li><div className="boardOptionItem" onClick={()=>{this.setState({activeItem : 1})}}>Harmonogram</div></li>
+                        <li><div className="boardOptionItem" onClick={()=>{this.setState({activeItem : 2})}}>Planowanie</div></li>
+                        <li><div className="boardOptionItem" onClick={()=>{this.setState({activeItem : 3})}}>Kolaboranci</div></li>
+                        <li><div className="boardOptionItem" onClick={()=>{this.setState({activeItem : 4})}}>Kosztorys</div></li>
+                        <li><div className="boardOptionItem" onClick={()=>{this.setState({activeItem : 5})}}>Publikacja</div></li>
                     </ul>
                 </div>
                 <div className="flex-1 bg-gray-200">
-                    <Colaboration></Colaboration>
+                    {this.renderProperItem()}
                 </div>
             </div>
         )
---
+++ src/components/Event/Publication.js
@@ -0,0 +1,16 @@
+import React, {Component} from 'react';
+
+class Publication extends Component{
+    state={
+
+    }
+    componentDidMount(){
+        
+    }
+    render(){
+        return(
+            <div>Publication</div>
+        )
+    }
+}
+export default Publication
