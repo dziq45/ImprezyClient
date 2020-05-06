b6521d3e1b6aadda7cc44aa86d70de1c2b77abed
Przemek <przwardzynski@gmail.com>
2020-05-07 01:01:09 +0200

    Resized optionBar

--- src/components/Event/EventBoard.js
+++ src/components/Event/EventBoard.js
@@ -35,7 +35,7 @@ class EventBoard extends Component{
         return(
             <div className="flex  mx-6">
                 <div className="flex-2 flex-grow-0">
-                    <ul className="stickyOptionBar">
+                    <ul className="stickyOptionBar text-2xl">
                         <li><div className={this.state.activeItem == 0? "boardOptionItemSelected": "boardOptionItem"} onClick={()=>{this.setState({activeItem : 0})}}>Ogólne</div></li>
                         <li><div className={this.state.activeItem == 1? "boardOptionItemSelected": "boardOptionItem"} onClick={()=>{this.setState({activeItem : 1})}}>Harmonogram</div></li>
                         <li><div className={this.state.activeItem == 2? "boardOptionItemSelected": "boardOptionItem"} onClick={()=>{this.setState({activeItem : 2})}}>Planowanie</div></li>
