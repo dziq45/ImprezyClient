7773b7abd0052aea9c07b221c09a8373b00993e2
dziq45 <piotr.papriko@gmail.com>
2020-06-16 20:48:54 +0200

    Changed api caller to https

--- src/apiCaller.js
+++ src/apiCaller.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 export const MyApiClient = axios.create({
-  baseURL: 'http://46.41.143.64:8080',
+  baseURL: 'https://46.41.143.64:8080',
   timeout: 2000,
   headers: {'X-Custom-Header': 'foobar', 'Content-Type': 'application/json','Access-Control-Allow-Origin':'*'}
 });
